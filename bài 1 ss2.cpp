a24d692c409f0ad164ccc616ee30cf06fedf40e8
namsondayma <147359790+namsondayma@users.noreply.github.com>
2023-11-08 06:00:27 +0700

    Add files via upload

---
+++ bài 1 ss2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	float cubic_edge = 4;
+	float length = 3;
+	float width = 5;
+	float radius = 6;
+	const float pi = 3.14;
+	float square_perimeter =  cubic_edge * 4;
+	float area_square = cubic_edge * cubic_edge;
+	float perimeter_of_a_rectangle = (length + width) * 2;
+	float rectangular_area = length * width;
+	float diameter_of_a_circle = 2 * radius * pi;
+	float circle_area = radius * radius * pi; 
+} 
